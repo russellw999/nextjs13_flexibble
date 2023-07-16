bdad6e2127048123d0bf68ac2113c3d68ec392a5
russellw999 <russellw999@gmail.com>
2023-07-16 10:33:14 +0200

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+          Navbar
+          <main>
+             {children}
+          </main>
+          Footer
+      </body>
     </html>
   )
 }
