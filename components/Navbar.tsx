d101aa379448f450f093c42dd09b922d4dce90c2
russellw999 <russellw999@gmail.com>
2023-07-16 14:54:13 +0200

    mion 36.04 completed Navbar and Footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import { NavLinks } from '@/constants';
+import Image from 'next/image'  ;
+import Link from 'next/link' ;
 
 const Navbar = () => {
+    const session = {};
   return (
-    <div>
-       Navbar
-    </div>
+    <nav className='flexBetween navbar'>
+       <div className="flex-1 flexStart gap-10">
+          <Link href='/'>
+             <Image
+                 src="/logo.svg"
+                 width={115}
+                 height={43}
+                 alt="Felxibble"
+              />
+          </Link>
+          <ul className="xl:flex hidden text-small gap-7">
+             {NavLinks.map((link) => (
+                <Link href={link.href} key={link.key}>
+                    {link.text}
+                </Link>
+             ))}
+          </ul>
+       </div>
+
+       <div className="flex-center gap-4">
+          {session ? (
+            <>
+              UserPhoto  
+
+              <Link href='/create-project'></Link>
+            </>
+          ) : (
+            <>
+              UserPhoto  
+
+                <Link href='/create-project'></Link>
+            </>
+        
+          )}
+       </div>
+
+    </nav>
   );
 }
 
