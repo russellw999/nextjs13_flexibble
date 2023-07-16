47bbf801a13f444f3e75384aa745759a17ba90cd
russellw999 <russellw999@gmail.com>
2023-07-16 11:29:03 +0200

    Layout with Navbar and Footer skeleton done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import "./globals.css";
+import { Navbar, Footer } from "@/components";
+
+
 export const metadata = {
   title: "Flexibble",
   description: "Showcase and discover remarkable developer projects",
@@ -10,7 +14,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+
+     </body>
     </html>
   )
 }
